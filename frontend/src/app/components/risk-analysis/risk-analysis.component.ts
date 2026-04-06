c2cd6bd7fe2dc584f169d8df28a5ec608a6f73e8
Ame Shajid <156400050+AmeShajid@users.noreply.github.com>
2026-04-06 00:29:12 -0400

    I added the new risk Analysis page, this part just includes the monte carlo risk basic components, after this I start actually building it

---
+++ frontend/src/app/components/risk-analysis/risk-analysis.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-risk-analysis',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './risk-analysis.component.html',
+  styleUrls: ['./risk-analysis.component.css']
+})
+export class RiskAnalysisComponent {}
